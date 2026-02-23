7be2b205f7ddda7658abbcd632266c7568773735
Daphne Greene <daphnergreene@gmail.com>
2026-02-22 16:08:38 -0800

    feat: import tutorial gameplay scripts and initial Unity wiring

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[DefaultExecutionOrder(-1)]
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private GameObject[] players;
+
+    private void Awake()
+    {
+        if (Instance != null) {
+            DestroyImmediate(gameObject);
+        } else {
+            Instance = this;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this) {
+            Instance = null;
+        }
+    }
+
+    private void Start()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
+
+    public void CheckWinState()
+    {
+        int aliveCount = 0;
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i].activeSelf) {
+                aliveCount++;
+            }
+        }
+
+        if (aliveCount <= 1) {
+            Invoke(nameof(NewRound), 3f);
+        }
+    }
+
+    private void NewRound()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
